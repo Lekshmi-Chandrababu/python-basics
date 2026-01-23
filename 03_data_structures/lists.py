eb9975c1f36ce6fcc858c7359480130d6dc37fda
Lekshmi Chandrababu <lekshmichandrababu186@gmail.com>
2026-01-23 21:53:13 +0530

    Add basic list operations in lists.py

---
+++ 03_data_structures/lists.py
@@ -0,0 +1,12 @@
+# lists.py
+
+numbers = [10, 20, 30, 40]
+
+print(numbers)
+print(numbers[0])      # first element
+print(numbers[-1])     # last element
+
+numbers.append(50)     # add element
+numbers.remove(20)     # remove element
+
+print(numbers)
