f5e75e73e5f1c815602f1233d871f7938d57eb97
Lekshmi Chandrababu <lekshmichandrababu186@gmail.com>
2026-01-28 20:51:40 +0530

    Add student dictionary with initial data and updates

---
+++ 03_data_structures/dictionaries.py
@@ -0,0 +1,15 @@
+# dictionaries.py
+
+student = {
+    "name": "Lekshmi",
+    "age": 21,
+    "cgpa": 9.2
+}
+
+print(student)
+print(student["name"])
+
+student["age"] = 22
+student["branch"] = "MCA"
+
+print(student)
