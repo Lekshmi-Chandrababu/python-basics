0b369664e755e236e2c6c7ccef76b3a135007279
Lekshmi Chandrababu <lekshmichandrababu186@gmail.com>
2026-02-02 20:28:49 +0530

    Create multiple_exceptions.py

---
+++ 08_exception_handling/multiple_exceptions.py
@@ -0,0 +1,10 @@
+# multiple_exceptions.py
+
+try:
+    a = int(input("Enter a number: "))
+    b = int(input("Enter another number: "))
+    print(a / b)
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+except ValueError:
+    print("Please enter numbers only")
